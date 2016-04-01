2fc6a5976ebde3ce72be022915c4dfb37ca7454f
richermans <heini89@gmail.com>
2016-04-01 16:47:18 +0800

    Finally got some time to modify install script

--- add_vpn_tables.sh
+++ add_vpn_tables.sh
@@ -7,8 +7,8 @@ HOME_NET=$HOME_NET
 VPN_DEV=$VPN_DEV
 
 start(){
-    echo "Enabling Routing for device "$VPN_DEV=$VPN_DEV" to address "$VPN_ADDR=$VPN_ADDR" from and to Network "$HOME_NET=$HOME_NET
-    ip route add default via $VPN_ADDR=$VPN_ADDR dev $VPN_DEV=$VPN_DEV table vpn
+    echo "Enabling Routing for device "$VPN_DEV" to address "$VPN_ADDR" from and to Network "$HOME_NET
+    ip route add default via $VPN_ADDR dev $VPN_DEV table vpn
     # Dunno why, but somehow lookup works, but table doesnt ... no idea why
     ip rule add to $HOME_NET=$HOME_NET lookup vpn
     ip rule add from $HOME_NET=$HOME_NET lookup vpn
@@ -16,8 +16,8 @@ start(){
 
 
 stop(){
-    ip route del default via $VPN_ADDR=$VPN_ADDR dev $VPN_DEV=$VPN_DEV table vpn
+    ip route del default via $VPN_ADDR dev $VPN_DEV table vpn
 
-    ip rule del to $HOME_NET=$HOME_NET
-    ip rule del from $HOME_NET=$HOME_NET
+    ip rule del to $HOME_NET
+    ip rule del from $HOME_NET
 }
--- install.sh
+++ install.sh
@@ -1,16 +1,78 @@
 #!/bin/sh
 
+
+
+
+
+
 SCRIPTNAME="add_vpn_tables.sh"
 # Add the VPN rule into IpTables
-echo "10    vpn" >> /etc/iproute2/rt_tables
 
 
-sed -i 's/VPN_ADDR/VPN_ADDR=$VPN_ADDR/g' $SCRIPTNAME
-sed -i 's/HOME_NET/HOME_NET=$HOME_NET/g' $SCRIPTNAME
-sed -i 's/VPN_DEV/VPN_DEV=$VPN_DEV/g' $SCRIPTNAME
+addVPNTable(){
+    echo "10    vpn" >> /etc/iproute2/rt_tables
+}
 
-cp $SCRIPTNAME /etc/init.d/
-chmod a+x /etc/init.d/$SCRIPTNAME
+replaceAddScript () {
+
+    tmpfile="tmpscriptfile"
+
+    touch $tmpfile
+
+    sed "s/VPN_ADDR=\$VPN_ADDR/VPN_ADDR=$VPN_ADDR/g" $SCRIPTNAME >> $tmpfile
+    sed -i "s/HOME_NET=\$HOME_NET/HOME_NET=$HOME_NET/g" $tmpfile
+    sed -i "s/VPN_DEV=\$VPN_DEV/VPN_DEV=$VPN_DEV/g" $tmpfile
+
+}
+
+VPN_ADDR="awd"
+HOME_NET=""
+VPN_DEV=""
+
+# Prompts to obtain the VPN_ADDR variable
+while true; do
+        echo -n "Please input the remote VPN address of the VPN!\t"
+        read answer
+        if echo "$answer" | grep -qE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ;then
+            VPN_ADDR=$answer
+            break;
+        else
+            echo "Given argument is not a ip adress, retry!"
+        fi
+done
+
+# Prompts to obtain the HOME_NET Variable
+while true; do
+        echo -n "Please input the current networks specified ip address, which the VPN is connected to!\t"
+        read answer
+        if echo "$answer" | grep -qE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ;then
+            HOME_NET=$answer
+            break;
+        else
+            echo "Given argument is not a ip adress, retry!"
+        fi
+    done
+# Prompts to obtain the VPN_DEV Variable
+while true; do
+        echo -n "Please input the local VPN device's name!\t"
+        read answer
+        if echo "$answer" | grep -qE "^[a-zA-Z]" ;then
+            VPN_DEV=$answer
+            break;
+        else
+            echo "Given argument is not a device name, retry!"
+        fi
+done
+
+echo "Checking for the VPN-Device to exist"
+
+ip a | grep -Eq ': $VPN_DEV:.*state UP'
+
+replaceAddScript
+
+
+# cp $SCRIPTNAME /etc/init.d/
+# chmod a+x /etc/init.d/$SCRIPTNAME
 
 
 
